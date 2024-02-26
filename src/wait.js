74a5ba0242368484606d5e76c2ef0c3941486b03
shrpne <shrpne@gmail.com>
2024-02-26 11:33:42 +0000

    add waitPromiseWithCondition

--- src/wait.js
+++ src/wait.js
@@ -26,3 +26,43 @@ export function waitCondition(conditionFn, delay = 100) {
         }, delay);
     });
 }
+
+/**
+ * @template {any} T
+ * @param {function(): Promise<T>} promiseFn
+ * @param {number} delay
+ * @param {object} [options]
+ * @param {number} [options.limit=0] - max number of checks
+ * @param {boolean} [options.immediate=false]
+ * @param {function(T): boolean} [options.validate]
+ * @return {Promise<T>}
+ */
+export async function waitPromiseWithCondition(promiseFn, delay, {limit = 0, immediate = false, validate} = {}) {
+    // by default just resolved promise is enough so no extra validation
+    validate = typeof validate === 'function' ? validate : () => true;
+
+    let counter = 0;
+    if (immediate) {
+        const result = await promiseFn();
+        if (validate(result)) {
+            return result;
+        }
+        counter = 1;
+    }
+
+    async function check() {
+        if (limit && counter >= limit) {
+            return false;
+        }
+        counter++;
+        await wait(delay);
+        const result = await promiseFn();
+        if (validate(result)) {
+            return result;
+        }
+        return check();
+    }
+
+    return check();
+}
+
--- types/src/wait.d.ts
+++ types/src/wait.d.ts
@@ -11,3 +11,18 @@ export function wait<T extends unknown>(time: number, result?: T): Promise<T>;
  * @return {Promise<void>}
  */
 export function waitCondition(conditionFn: () => boolean, delay?: number): Promise<void>;
+/**
+ * @template {any} T
+ * @param {function(): Promise<T>} promiseFn
+ * @param {number} delay
+ * @param {object} [options]
+ * @param {number} [options.limit=0] - max number of checks
+ * @param {boolean} [options.immediate=false]
+ * @param {function(T): boolean} [options.validate]
+ * @return {Promise<T>}
+ */
+export function waitPromiseWithCondition<T extends unknown>(promiseFn: () => Promise<T>, delay: number, { limit, immediate, validate }?: {
+    limit?: number;
+    immediate?: boolean;
+    validate?: (arg0: T) => boolean;
+}): Promise<T>;
